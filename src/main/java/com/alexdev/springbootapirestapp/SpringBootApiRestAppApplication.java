687ff0111e14fda510efdbf8a6f3959fe0acf41f
alex <alexxxd23@gmail.com>
2024-02-16 13:41:17 -0300

    add project created from spring initializr

---
+++ src/main/java/com/alexdev/springbootapirestapp/SpringBootApiRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.alexdev.springbootapirestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexdev/springbootapirestapp/SpringBootApiRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdev.springbootapirestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
