c1570a4597c291ede4587c84489037c20a3514a8
alex <alexxxd23@gmail.com>
2024-03-06 16:55:27 -0300

    add first endpoint

---
+++ src/main/java/com/alexdev/springbootapirestapp/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.alexdev.springbootapirestapp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
